64ecbe8df6e76c002a9b43f375c4520d71b7dd30
kenliii <90875261+kenliii@users.noreply.github.com>
2022-05-02 18:06:33 -0700

    routes

--- server.js
+++ server.js
@@ -27,3 +27,20 @@ const PORT = process.env.PORT || 8080;
 app.set('view engine', 'ejs')
 
 //routes
+
+app.use("/jobs", routes.jobs );
+app.use("/jobs", routes.commentsJobs );
+app.use("/events", routes.events );
+app.use("/events", routes.commentsEvents );
+app.use("/pets", routes.pets );
+app.use("/pets", routes.commentsPets );
+app.use("/music", routes.music );
+app.use("/music", routes.commentsMusic );
+app.use("/paraphernalia", routes.paraphernalia );
+app.use("/paraphernalia", routes.commentsParaphernalia );
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", (req,res)=> {
+    res.render(index);
+});
