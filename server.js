8f0544c6efa2a1c50796fa9a8354c9b69df89fc8
kenliii <90875261+kenliii@users.noreply.github.com>
2022-05-02 17:39:00 -0700

    internal mods

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+//internal mods
+
+require('./config/database');
+const routes = require('./routes/index');
+
